7a931d6e6a8cf876d7f2bcba26a542266558003a
Andrey Chasovskoy <andrchas777@gmail.com>
2023-01-04 11:28:11 +0200

    code with function

--- function_for_pow.py
+++ function_for_pow.py
@@ -2,4 +2,10 @@ from math import pow
 
 
 def wrapper(arg1, arg2):
-    pass
+    if not(isinstance(arg1, (int, float)) and isinstance(arg2, (int, float))):
+        msg = "must be int or float, given " + str(type(arg1))
+        raise TypeError(msg)
+    return pow(float(arg1), float(arg2))
+
+
+assert(wrapper(2, 3)) == 8.0
